0ee21866f55928c5efd4baa708858c4d76850001
Genae <stefan@2stark4you.de>
2018-11-04 17:23:22 +0100

    Allow Weapons to Be Equipped

--- SundayJam/Assets/Scripts/PlayerController.cs
+++ SundayJam/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using Assets.Scripts.Weapons;
+using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public WeaponBase WeaponToEquip;
+    private WeaponBase _equipedWeapon;
 
     private float _playerSpeed = 6;
 
@@ -17,6 +21,13 @@ public class PlayerController : NetworkBehaviour
 
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * _playerSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * _playerSpeed;
+        if (WeaponToEquip != null)
+        {
+            EquipWeapon(WeaponToEquip);
+            WeaponToEquip = null;
+        }
+
+
 
         Plane playerPlane = new Plane(Vector3.up, transform.position);
 
@@ -36,17 +47,38 @@ public class PlayerController : NetworkBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            CmdFire();
+            _equipedWeapon.ButtonDown();
         }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _equipedWeapon.ButtonUp();
+        }
+
+        _equipedWeapon.RunUpdate(Time.deltaTime);
+    }
+
+    public void EquipWeapon(WeaponBase weaponBase)
+    {
+        _equipedWeapon = weaponBase;
+        _equipedWeapon.player = this;
+        this.bulletPrefab = weaponBase.bulletPrefab;
+    }
+
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+        EquipWeapon(Resources.FindObjectsOfTypeAll<WeaponBase>().First());
     }
 
     // This [Command] code is called on the Client …
     // … but it is run on the Server!
     [Command]
-    void CmdFire()
+    public void CmdFire()
     {
         // Create the Bullet from the Bullet Prefab
-        var bullet = (GameObject)Instantiate(
+        var bullet = (GameObject) Instantiate(
             bulletPrefab,
             bulletSpawn.position,
             bulletSpawn.rotation);
@@ -59,10 +91,5 @@ public class PlayerController : NetworkBehaviour
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
-    }
-
-    public override void OnStartLocalPlayer()
-    {
-        GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 }
---
+++ SundayJam/Assets/Scripts/Weapons/WeaponBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.Weapons
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "SO/Weapon", order = 1)]
+    public class WeaponBase : ScriptableObject
+    {
+        public bool Automatic;
+        public float Cooldown;
+        public int MaxAmmunition;
+        public int CurrentAmmunition;
+
+        public GameObject bulletPrefab;
+        public PlayerController player;
+
+        protected bool _shooting;
+        protected float _currentCooldown;
+
+        public void ButtonDown()
+        {
+            Debug.Log("Start");
+            if (Automatic)
+            {
+                _shooting = true;
+            }
+            else
+                DoShooting();
+        }
+
+        public void ButtonUp()
+        {
+            Debug.Log("Stop");
+            _shooting = false;
+        }
+
+        public void RunUpdate(float deltaTime)
+        {
+            DoAutomaticShooting(deltaTime);
+        }
+
+        private void DoAutomaticShooting(float deltaTime)
+        {
+            if (_currentCooldown > 0)
+            {
+                _currentCooldown -= deltaTime;
+                return;
+            }
+
+            if (CurrentAmmunition == 0)
+            {
+                return;
+            }
+
+            if (!_shooting)
+                return;
+
+            _currentCooldown = Cooldown;
+            player.CmdFire();
+        }
+
+        private void DoShooting()
+        {
+            player.CmdFire();
+        }
+
+        
+    }
+}
